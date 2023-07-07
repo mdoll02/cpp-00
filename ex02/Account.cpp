6972eed9d9da3f4f7a68c1e071c781db0562ef9f
mdoll <mdoll@student.42wolfsburg.de>
2023-07-07 13:22:36 +0200

    feat: implemented all needed functions (not completely correct)

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -11,13 +11,98 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
-Account::Account( int initial_deposit ) : _amount(initial_deposit), _accountIndex(_nbAccounts + 1), _nbDeposits(0), _nbWithdrawals(0)
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
 {
-	-_nbAccounts++;
+	_nbAccounts++;
+	_accountIndex = _nbAccounts + 1;
+	_totalAmount += initial_deposit;
 }
 
-Account::~Account( void )
+Account::~Account()
 {
 
+}
+
+Account::Account()
+{
+
+}
+
+int	Account::getNbAccounts()
+{
+	return(_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return(_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return(_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return(_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp()
+{
+	time_t	t;
+	tm	*tm = localtime(&t);
+
+	std::cout << tm->tm_year;
+	std::cout << tm->tm_mon;
+	std::cout << tm->tm_mday;
+	std::cout << tm->tm_hour;
+	std::cout << tm->tm_sec;
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts() << ";total:" << getTotalAmount();
+	std::cout << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout <<  " index:" << _accountIndex << ";amount:" << _amount << ";deposits:";
+	std::cout << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+}
+
+int Account::checkAmount() const
+{
+	return(this->_amount);
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (checkAmount() - withdrawal >= 0)
+	{
+		this->_amount -= withdrawal;
+		_totalNbWithdrawals++;
+		_totalAmount -= withdrawal;
+		return (true);
+	}
+	else
+		return (false);
 }
